1ef45be8131b457c8d17134ff405ebcfd666364a
OCI DEX Release Bot Automation <oci-dex-releases-bot_ww@oracle.com>
2026-02-04 20:56:05 +0000

    Releasing version 123.1.0
    Co-authored-by: Harsh Kumar <h.harsh.kumar@oracle.com>

--- Common/Version.cs
+++ Common/Version.cs
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020, 2026, Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2024, Oracle and/or its affiliates.
  * This software is dual-licensed to you under the Universal Permissive License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl or Apache License 2.0 as shown at http://www.apache.org/licenses/LICENSE-2.0. You may choose either license.
  */
 namespace Oci.PSModules.Common.Cmdlets
@@ -7,7 +7,7 @@ namespace Oci.PSModules.Common.Cmdlets
     public class Version
     {
         public static string MAJOR = "123";
-        public static string MINOR = "0";
+        public static string MINOR = "1";
         public static string PATCH = "0";
 
         public static string GetVersion()
